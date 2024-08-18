7819978bd1b35223ee338127b08861cf1574b8f9
everettedeb <161762074+everettedeb@users.noreply.github.com>
2024-08-18 07:43:27 +0200

    Update MineLib.sol

--- contracts/miner/MineLib.sol
+++ contracts/miner/MineLib.sol
@@ -23,8 +23,9 @@ library MineLib {
         uint skipSeals,
         bytes32[2] memory padDigest
     ) internal view returns (bytes32[2] memory recallSeed, bytes32[UNITS_PER_SEAL] memory mixedData) {
-        for (uint i = 0; i < skipSeals; i += 1) {
-            for (uint j = 0; j < BHASHES_PER_SEAL; j += 1) {
+        // Compute the scratch pad and mix data for the mining process
+        for (uint i = 0; i < skipSeals; i++) {
+            for (uint j = 0; j < BHASHES_PER_SEAL; j++) {
                 padDigest = Blake2b.blake2b(padDigest);
             }
         }
@@ -35,8 +36,8 @@ library MineLib {
             mixedData[i + 1] = padDigest[1] ^ sealedData[i + 1];
         }
 
-        for (uint i = skipSeals + 1; i < SEALS_PER_PAD; i += 1) {
-            for (uint j = 0; j < BHASHES_PER_SEAL; j += 1) {
+        for (uint i = skipSeals + 1; i < SEALS_PER_PAD; i++) {
+            for (uint j = 0; j < BHASHES_PER_SEAL; j++) {
                 padDigest = Blake2b.blake2b(padDigest);
             }
         }
@@ -49,16 +50,15 @@ library MineLib {
         bytes32[2] memory padSeed,
         bytes32[UNITS_PER_SEAL] memory mixedData
     ) internal view returns (bytes32) {
-        bytes32[2] memory h;
-        h[0] = Blake2b.BLAKE2B_INIT_STATE0;
-        h[1] = Blake2b.BLAKE2B_INIT_STATE1;
+        // Compute the PoRA (Proof of Random Access) hash for sealing
+        bytes32[2] memory h = [
+            Blake2b.BLAKE2B_INIT_STATE0,
+            Blake2b.BLAKE2B_INIT_STATE1
+        ];
 
         h = Blake2b.blake2bF(h, bytes32(sealOffset), padSeed[0], padSeed[1], bytes32(0), 128, false);
         for (uint i = 0; i < UNITS_PER_SEAL - 4; i += 4) {
-            uint length;
-            unchecked {
-                length = 128 + 32 * (i + 4);
-            }
+            uint length = 128 + 32 * (i + 4);
             h = Blake2b.blake2bF(h, mixedData[i], mixedData[i + 1], mixedData[i + 2], mixedData[i + 3], length, false);
         }
         h = Blake2b.blake2bF(
@@ -74,10 +74,10 @@ library MineLib {
     }
 
     function unseal(PoraAnswer memory answer) internal pure returns (bytes32[UNITS_PER_SEAL] memory unsealedData) {
-        unsealedData[0] =
-            answer.sealedData[0] ^
+        // Unseal the sealed data using the miner's ID and sealed context digest
+        unsealedData[0] = answer.sealedData[0] ^
             keccak256(abi.encode(answer.minerId, answer.sealedContextDigest, answer.recallPosition));
-        for (uint i = 1; i < UNITS_PER_SEAL; i += 1) {
+        for (uint i = 1; i < UNITS_PER_SEAL; i++) {
             unsealedData[i] = answer.sealedData[i] ^ keccak256(abi.encode(answer.sealedData[i - 1]));
         }
     }
@@ -86,49 +86,25 @@ library MineLib {
         PoraAnswer memory answer,
         bytes32[UNITS_PER_SEAL] memory unsealedData
     ) internal pure returns (bytes32) {
-        // console.log("Compute leaf");
-        // Compute leaf of hash
+        // Recover the Merkle root from the unsealed data and the provided Merkle proof
         for (uint i = 0; i < UNITS_PER_SEAL; i += UNITS_PER_SECTOR) {
-            bytes32 x;
-            assembly {
-                x := keccak256(add(unsealedData, mul(i, 32)), 256 /*BYTES_PER_SECTOR*/)
-            }
-            unsealedData[i] = x;
-            // console.logBytes32(x);
+            unsealedData[i] = keccak256(abi.encodePacked(unsealedData, i * 32));
         }
 
         for (uint i = UNITS_PER_SECTOR; i < UNITS_PER_SEAL; i <<= 1) {
-            // console.log("i=%d", i);
             for (uint j = 0; j < UNITS_PER_SEAL; j += i << 1) {
-                bytes32 left = unsealedData[j];
-                bytes32 right = unsealedData[j + i];
-                unsealedData[j] = keccak256(abi.encode(left, right));
-                // console.logBytes32(unsealedData[j]);
+                unsealedData[j] = keccak256(abi.encodePacked(unsealedData[j], unsealedData[j + i]));
             }
         }
+
         bytes32 currentHash = unsealedData[0];
         delete unsealedData;
 
-        // console.log("Seal root");
-        // console.logBytes32(currentHash);
-
         uint unsealedIndex = answer.recallPosition / SECTORS_PER_SEAL;
-
-        for (uint i = 0; i < answer.merkleProof.length; i += 1) {
-            bytes32 left;
-            bytes32 right;
-            if (unsealedIndex % 2 == 0) {
-                left = currentHash;
-                right = answer.merkleProof[i];
-            } else {
-                left = answer.merkleProof[i];
-                right = currentHash;
-            }
-            currentHash = keccak256(abi.encode(left, right));
-            // console.log("sibling");
-            // console.logBytes32(answer.merkleProof[i]);
-            // console.log("current");
-            // console.logBytes32(currentHash);
+        for (uint i = 0; i < answer.merkleProof.length; i++) {
+            currentHash = unsealedIndex % 2 == 0
+                ? keccak256(abi.encodePacked(currentHash, answer.merkleProof[i]))
+                : keccak256(abi.encodePacked(answer.merkleProof[i], currentHash));
             unsealedIndex /= 2;
         }
         return currentHash;
